fd0ab0963b1d7f2ff74d94d1ccbd21c55f8ccdc1
Anubhav Ranjan <60106478+Mr-AB007@users.noreply.github.com>
2021-02-17 14:01:00 +0530

    Create FibandFActorial.java

---
+++ Assignment2/FibandFActorial.java
@@ -0,0 +1,37 @@
+//Fibonacci Numbers usimg recursion
+
+class Fibonacci{  
+ static int n1=0,n2=1,n3=0;    
+ static void printFibonacci(int count){    
+    if(count>0){    
+         n3 = n1 + n2;    
+         n1 = n2;    
+         n2 = n3;    
+         System.out.print(" "+n3);   
+         printFibonacci(count-1);    
+     }    
+ }    
+ public static void main(String args[]){    
+  int count=10;    
+  System.out.print(n1+" "+n2);//printing 0 and 1    
+  printFibonacci(count-2);//n-2 because 2 numbers are already printed   
+ }  
+} 
+
+
+//Factorial using Recursion
+
+class Factorial{  
+ static int factorial(int n){    
+  if (n == 0)    
+    return 1;    
+  else    
+    return(n * factorial(n-1));    
+ }    
+ public static void main(String args[]){  
+  int i,fact=1;  
+  int number=4;//It is the number to calculate factorial    
+  fact = factorial(number);   
+  System.out.println("Factorial of "+number+" is: "+fact);    
+ }  
+} 
