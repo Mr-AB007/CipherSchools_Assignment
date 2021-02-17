3888625fd12c3a32277f5d450cdee0fb7b01a974
Anubhav Ranjan <60106478+Mr-AB007@users.noreply.github.com>
2021-02-17 13:37:15 +0530

    Update CountInversions.java

--- Assignment1/CountInversions.java
+++ Assignment1/CountInversions.java
@@ -1,18 +1,73 @@
-class Inversions {
-    static int arr[] = new int[] { 1, 20, 6, 4, 5 };
- 
-    static int getInvCount(int n)
+//Counting Inversions Problem
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CountInversion {
+    
+    private static int mergeAndCount(int[] arr, int l,
+                                     int m, int r)
     {
-        int inv_count = 0;
-        for (int i = 0; i < n - 1; i++)
-            for (int j = i + 1; j < n; j++)
-                if (arr[i] > arr[j])
-                    inv_count++;
- 
-        return inv_count;
+
+        // Left subarray
+        int[] left = Arrays.copyOfRange(arr, l, m + 1);
+
+        // Right subarray
+        int[] right = Arrays.copyOfRange(arr, m + 1, r + 1);
+
+        int i = 0, j = 0, k = l, swaps = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j])
+                arr[k++] = left[i++];
+            else {
+                arr[k++] = right[j++];
+                swaps += (m + 1) - (l + i);
+            }
+        }
+        while (i < left.length)
+            arr[k++] = left[i++];
+        while (j < right.length)
+            arr[k++] = right[j++];
+        return swaps;
     }
+
+    // Merge sort function
+    private static int mergeSortAndCount(int[] arr, int l,
+                                         int r)
+    {
+        
+        int count = 0;
+
+        if (l < r) {
+            int m = (l + r) / 2;
+            
+            // Left subarray count
+            count += mergeSortAndCount(arr, l, m);
+
+            // Right subarray count
+            count += mergeSortAndCount(arr, m + 1, r);
+
+            // Merge count
+            count += mergeAndCount(arr, l, m, r);
+        }
+
+        return count;
+    }
+
+    // Driver code
     public static void main(String[] args)
     {
-        System.out.println("Inversions: "+ getInvCount(arr.length));
+        System.out.println("Enter Size");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+        System.out.println("Enter the array element");
+        for(int i =0; i < n; i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+
+        System.out.println(
+                mergeSortAndCount(arr, 0, arr.length - 1));
     }
 }
