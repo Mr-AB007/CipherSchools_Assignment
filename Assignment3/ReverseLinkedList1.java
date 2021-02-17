2dce34d0c3840aa3b1adc24326f51a620ee8b0bf
Anubhav Ranjan <60106478+Mr-AB007@users.noreply.github.com>
2021-02-17 17:41:44 +0530

    Create ReverseLinkedList1.java

---
+++ Assignment3/ReverseLinkedList1.java
@@ -0,0 +1,47 @@
+//Reversing linked list with iteration
+
+package Assignment3;
+
+class ReverseLinkedList1 {
+
+    /* Function to reverse the linked list */
+    static Node reverse(Node node)
+    {
+        Node prev = null;
+        Node current = node;
+        Node next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        node = prev;
+        return node;
+    }
+
+    // prints content of double linked list
+    static void printList(Node node)
+    {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+
+    // Driver Code
+    public static void main(String[] args)
+    {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+
+        System.out.println("Given Linked list");
+        printList(head);
+        head = reverse(head);
+        System.out.println("");
+        System.out.println("Reversed linked list ");
+        printList(head);
+    }
+}
