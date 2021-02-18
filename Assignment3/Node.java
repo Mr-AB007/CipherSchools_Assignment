665ce801d86d1145855874d8d79889115e342911
Anubhav Ranjan <60106478+Mr-AB007@users.noreply.github.com>
2021-02-18 15:27:49 +0530

    Create Node.java

---
+++ Assignment3/Node.java
@@ -0,0 +1,11 @@
+//this class for creating all Linked list Node
+public class Node {
+
+
+    public int data;
+
+   public Node next;
+
+    public Node(int data) {
+        this.data = data;
+    }
